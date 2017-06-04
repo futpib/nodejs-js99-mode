fd165eb182ae6aa29848f5fdb92200be412f60a9
futpib <futpib@gmail.com>
2017-06-04 20:52:53 +0300

    Enable all the babylon plugins and stuff

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ const {defaults} = require('lodash/fp');
 
 const babylon = require('babylon');
 
-const DEFAULT_PARSE_OPTIONS = {
-	sourceType: 'module'
+const defaultOptions = {
+	sourceType: 'module',
+	allowImportExportEverywhere: false,
+	allowReturnOutsideFunction: false,
+	plugins: [
+		'asyncGenerators',
+		'classConstructorCall',
+		'classProperties',
+		'decorators',
+		'doExpressions',
+		'exportExtensions',
+		'flow',
+		'functionSent',
+		'functionBind',
+		'jsx',
+		'objectRestSpread',
+		'dynamicImport'
+	]
 };
 
 function parse(code, options) {
-	return babylon.parse(code, defaults(options, DEFAULT_PARSE_OPTIONS));
+	return babylon.parse(code, defaults(options, defaultOptions));
 }
 
 module.exports = {parse};
